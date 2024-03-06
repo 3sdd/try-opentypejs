30e47f1813417c34048bf13dd687688424b108a3
3sdd <56486103+3sdd@users.noreply.github.com>
2024-03-06 22:54:25 +0900

    フォントファミリーなどの情報表示

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ async function main() {
   );
   const font = parse(fontData.buffer);
   console.log(font.hasChar("鑠"));
+
+  console.log(font.names.fontFamily);
+  console.log(font.names.version);
+  console.log(font.names.description);
+
+  // Font.getPath(text, x, y, fontSize, options)
+  // Font.draw(ctx, text, x, y, fontSize, options)
+  // Font.drawPoints(ctx, text, x, y, fontSize, options)
+  // Font.drawMetrics(ctx, text, x, y, fontSize, options)
+  // Font.stringToGlyphs(string)
+  // Font.charToGlyph(char)
+  // Font.getKerningValue(leftGlyph, rightGlyph)
+  // Font.getAdvanceWidth(text, fontSize, options)
 }
 
 await main();
